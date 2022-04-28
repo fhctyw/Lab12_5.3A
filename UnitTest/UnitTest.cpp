079cce063da94591be8bdad0c256c699edd23fbf
Arsen <lisnychukarsenw@gmail.com>
2022-04-28 23:56:29 +0300

    Add project files.

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Lab12_5.3A\main.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Student s = { "name", 2, Spec::IT, 3, 4.5, 5 };
+			Assert::AreEqual((int)avgMark(s), 4);
+		}
+	};
+}
